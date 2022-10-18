394da6758fa2ce69663a82cfbb3f6f3fd25b5334
PauloContrera <paulocontrera97@gmail.com>
2022-10-18 03:38:45 -0300

    1 conexión con api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogosComponent } from './components/logos/logos.component';
+import { HomeComponent } from './components/home/home.component';
+import {HttpClientModule} from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AcercaDeComponent,
+    BannerComponent,
+    EducacionComponent,
+    ExperienciaComponent,
+    HeaderComponent,
+    LogosComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
