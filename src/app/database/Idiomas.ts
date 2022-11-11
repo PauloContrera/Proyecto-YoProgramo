4a3ae84c5bdd31af5d590fed34b598a2c89a91ec
PauloContrera <paulocontrera97@gmail.com>
2022-11-11 10:45:58 -0300

    sin español

--- src/app/database/Idiomas.ts
+++ src/app/database/Idiomas.ts
@@ -1,11 +1,6 @@
 
 export const Idiomas = [
     {
-        "id": 1,
-        "nombre": "Español",
-        "valor": 100
-    },
-    {
         "id": 2,
         "nombre": "Ingles",
         "valor": 40
